f95f798ca4414f791bd7d722f468af261d4d8df5
jonushzw <jonushzw@gmail.com>
2025-01-14 11:54:13 +0800

    Update eslint 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,6 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: true, // Ensure recommendedConfig is passed
 });
 
 const eslintConfig = [
